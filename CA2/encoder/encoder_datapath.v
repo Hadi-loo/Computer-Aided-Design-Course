1b09ec1580368c7ea9c45302c32e8bfda9c41561
Hadi <m.h.babalu@gmail.com>
2023-02-02 16:34:21 +0330

    added revaluateTop module

--- CA2/encoder/encoder_datapath.v
+++ CA2/encoder/encoder_datapath.v
@@ -3,7 +3,9 @@
 module encoder_datapath(clk, rst,
                         file_index, iteration,
                         CP_start, CP_finish,
+                        RO_start, RO_finish,
                         PE_start, PE_finish,
+                        RE_start, RE_finish,
                         RC_start, RC_finish);
     
     input clk, rst;
@@ -13,24 +15,27 @@ module encoder_datapath(clk, rst,
     output CP_finish, PE_finish, RC_finish;
 
     // colParity module
-    colParity CP (.clk(clk), .rst(rst), 
-                    .file_index(file_index), .start(CP_start), .finish(CP_finish));
+    colParity CP (  .clk(clk), .rst(rst),  
+                    .start(CP_start), .finish(CP_finish),
+                    .file_index(file_index));
 
-    // here comes rotate module
-
+    // rotate module
+    rotate RO (     .clk(clk), 
+                    .start(RO_start), .finish(RO_finish), 
+                    .file_index(file_index));
 
     // permute module
-    permute PE (.clk(clk), 
-                .start(PE_start), .finish(PE_finish), 
-                .file_index(file_index));
+    permute PE (    .clk(clk), 
+                    .start(PE_start), .finish(PE_finish), 
+                    .file_index(file_index));
     
-    // here comes revaluate module
+    // revaluate module
     
 
     // addRC module
-    addRC RC (  .clk(clk),
-                .start(RC_start), .finish(RC_finish),
-                .file_index(file_index), .iteration(iteration));
+    addRC RC (      .clk(clk),
+                    .start(RC_start), .finish(RC_finish),
+                    .file_index(file_index), .iteration(iteration));
     
 
 endmodule
---
+++ CA2/revaluate/revaluateTop_controller.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ns
+
+`define idle            3'd0
+`define init            3'd1
+`define read            3'd2
+`define cal             3'd3
+`define write           3'd4
+`define done            3'd5
+
+module revaluateTop_controller (clk, rst, start, read_file, write_file, finish, revaluate_start, revaluate_done);
+
+    input clk, start;
+    input revaluate_done;
+    
+    output reg read_file, write_file;
+    output reg rst, finish;
+    output revaluate_start;
+
+    reg [2:0] pstate = `idle;
+    reg [2:0] nstate;
+
+    always @(pstate, start, revaluate_done) begin
+        {rst, read_file, write_file, finish, revaluate_start} = 5'b0;
+        case (pstate)
+            `idle: begin nstate = (start) ? `init : `idle; end
+            `init: begin nstate = `read; rst = 1'b1; read_file = 1'b1; end
+            `read: begin nstate = `cal; revaluate_start = 1'b1; end
+            `cal: begin nstate = (revaluate_done) ? `write : `cal; end
+            `write: begin nstate = `done; write_file = 1'b1; end
+            `done: begin nstate = `idle; finish = 1'b1; end
+            default: begin nstate = `idle; end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        pstate <= nstate;
+    end
+
+endmodule
